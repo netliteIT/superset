9296b18c4b5ce5c4161b4d1828531c554e588e3c
Andrea Gagliardi <andrea@netlite.it>
2024-02-27 10:04:38 +0100

    redis_cache.py

--- redis_config.py
+++ redis_config.py
@@ -2,5 +2,19 @@ CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
     'CACHE_DEFAULT_TIMEOUT': 300,
     'CACHE_KEY_PREFIX': 'superset_results',
-    'CACHE_REDIS_URL': 'redis://localhost:6379/0'
+    'CACHE_REDIS_URL': 'redis://redis:6379/0'
+}
+
+FILTER_STATE_CACHE_CONFIG = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_DEFAULT_TIMEOUT': 86400, # 24 hours in seconds
+    'CACHE_KEY_PREFIX': 'superset_filter_cache',
+    'CACHE_REDIS_URL': 'redis://redis:6379/0'
+}
+
+EXPLORE_FORM_DATA_CACHE_CONFIG = {
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_DEFAULT_TIMEOUT': 86400,
+    'CACHE_KEY_PREFIX': 'superset_explore_cache',
+    'CACHE_REDIS_URL': 'redis://redis:6379/0'
 }
